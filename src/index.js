8f09776ff406caabc3c5f893570bf6134129f082
Alexandr <pythonloveyou@gmail.com>
2020-06-02 18:19:38 +0300

    Change json file name and chan it directory

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const requestURL = "https://drive.google.com/file/d/1DPILoLZ1Pz12DhfnxGy8eQItxUIK2DCB/";
+const requestURL = "https://github.com/PyGod/Redlab/blob/master/src/data.json";
 
 const xhr = new XMLHttpRequest();
 
